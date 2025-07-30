b58677190c66b674fb8748083c00b44609870792
rayantraore <rayan.traore@econocom.com>
2025-07-30 14:20:49 +0200

    chore: update .gitignore to include dist and node_modules for create-fastymini-app

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -1,15 +1,28 @@
 import type { UserConfig } from '@commitlint/types';
 
 const Configuration: UserConfig = {
-  extends: ['@commitlint/config-conventional'],
-  parserPreset: 'conventional-changelog-atom',
-  formatter: '@commitlint/format',
+  extends: ["@commitlint/config-conventional"],
+  parserPreset: "conventional-changelog-atom",
+  formatter: "@commitlint/format",
   rules: {
-    'type-enum': [
+    "type-enum": [
       2,
-      'always',
-      ['feat', 'fix', 'docs', 'chore', 'style', 'refactor', 'ci', 'test', 'revert', 'perf', 'vercel'],
+      "always",
+      [
+        "feat",
+        "fix",
+        "docs",
+        "chore",
+        "style",
+        "refactor",
+        "ci",
+        "test",
+        "revert",
+        "perf",
+        "vercel",
+      ],
     ],
+    "body-max-line-length": [2, "always", 200],
   },
 };
 
